ba576c5e22df93f4a7ad6b0104d7915caa87be51
koushith <koushith97@gmail.com>
2023-05-04 05:34:25 +0530

    made product cards responsive

--- src/pages/products/products.style.ts
+++ src/pages/products/products.style.ts
@@ -1,3 +1,29 @@
+// import styled from "styled-components"
+
+// export const ProductsPageContainer = styled.div`
+//   .top-filter {
+//     margin: 2rem 0;
+//     h2 {
+//       font-size: 1.6rem;
+//       font-weight: 700;
+//       color: #282c3f;
+//       span {
+//         font-weight: 300;
+//       }
+//     }
+//   }
+
+//   .main-container {
+//     display: flex;
+//     justify-content: space-between;
+//     gap: 6rem;
+//     .products {
+//       display: grid;
+//       grid-template-columns: repeat(5, 1fr);
+//     }
+//   }
+// `
+
 import styled from "styled-components"
 
 export const ProductsPageContainer = styled.div`
@@ -20,6 +46,26 @@ export const ProductsPageContainer = styled.div`
     .products {
       display: grid;
       grid-template-columns: repeat(5, 1fr);
+    }
+  }
+
+  @media (max-width: 48em) {
+    .main-container {
+      flex-direction: column;
+      gap: 2rem;
+      .products {
+        grid-template-columns: repeat(4, 1fr);
+      }
+    }
+  }
+
+  @media (max-width: 28em) {
+    .main-container {
+      flex-direction: column;
+      gap: 2rem;
+      .products {
+        grid-template-columns: repeat(2, 1fr);
+      }
     }
   }
 `
